908f25de235935fb946d379f109d49cb2df72e86
Pablo Nuñez <pablonura91@gmail.com>
2019-03-01 11:16:43 +0100

    Create BallGraphics.cs

---
+++ LibraryClass/BallGraphics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Game
+{
+    public class BallGraphics
+    {
+        public Ellipse ShapeBall { get; set; }
+        public Color Color { get; set; }
+
+        public BallGraphics(int width, int height, Color color)
+        {
+            ShapeBall = new Ellipse();
+            ShapeBall.Width = width;
+            ShapeBall.Height = height;
+            Color = color;
+            ShapeBall.Fill = new SolidColorBrush(Color);
+
+        }
+    }
+}
