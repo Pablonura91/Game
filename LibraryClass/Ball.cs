8239cba21fdea44490a0e6987c040c82a5d711b7
Pablo Nuñez <pablonura91@gmail.com>
2019-03-01 11:16:29 +0100

    Create Ball.cs

---
+++ LibraryClass/Ball.cs
@@ -0,0 +1,37 @@
+﻿using Game;
+using System;
+using System.Windows.Media;
+
+namespace Game
+{
+    public class Ball
+    {
+        //public int PosX { get; set; }
+        //public int PosY { get; set; }
+        //public int Width { get; set; }
+        //public int Height { get; set; }
+        public Color color { get; set; }
+        //public BallGraphics BallDraw;
+
+        public Ball() { }
+
+        public Ball(Color color)
+        {
+            //PosX = posX;
+            //PosY = posY;
+            //Width = width;
+            //Height = height;
+            this.color = color;
+
+            //BallDraw = new BallGraphics(width, height, color);
+        }
+
+        //public Ball(int posX, int posY, int width, int height)
+        //{
+        //    PosX = posX;
+        //    PosY = posY;
+        //    Width = width;
+        //    Height = height;            
+        //}
+    }
+}
